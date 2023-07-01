6ffdbd8f886f3a172cceae7c3edf593a92ff8f3d
Madhu Balan T <120023469+madhubalan17js@users.noreply.github.com>
2023-07-01 13:01:58 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -2,7 +2,7 @@
    # This script calculates simple interest given principal,
    # annual rate of interest and time period in years.
    # Do not use this in production. Sample purpose only.
-   # Author: Upkar Lidder (IBM)
+   # Author:Madhu Balan T
    # Additional Authors:
    # <madhubalan17js>
    # Input:
